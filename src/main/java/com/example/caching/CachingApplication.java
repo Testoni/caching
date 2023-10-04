dedc1019c2a735715932a10d47a8d8575574bed8
gabriel testoni <gabriel.testoni@gmail.com>
2023-10-03 23:43:30 -0300

    first commit

---
+++ src/main/java/com/example/caching/CachingApplication.java
@@ -0,0 +1,68 @@
+package com.example.caching;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableCaching
+public class CachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner runner(ProductService productService) {
+        return args -> {
+            System.out.println("\n\n\n\n\n\n\n\n\n");
+            System.out.println("ID: 1: " + productService.getById(1L));
+            System.out.println("ID: 2: " + productService.getById(2L));
+            System.out.println("ID: 1: " + productService.getById(1L));
+            System.out.println("ID: 1: " + productService.getById(1L));
+            System.out.println("ID: 1: " + productService.getById(1L));
+        };
+    }
+
+}
+
+record Product(Long id, String name, String description) implements Serializable {
+
+}
+
+@Service
+class ProductService {
+    Map<Long, Product> products = new HashMap<>() {
+        {
+            put(1L, new Product(1L, "A", "Test A"));
+            put(2L, new Product(2L, "B", "Test B"));
+            put(3L, new Product(3L, "C", "Test C"));
+            put(4L, new Product(4L, "D", "Test D"));
+            put(5L, new Product(5L, "E", "Test E"));
+        }
+    };
+
+    @Cacheable("products")
+    Product getById(Long id) {
+        System.out.println("Searching product...");
+        simulateLatency();
+        return products.get(id);
+    }
+
+    private void simulateLatency() {
+        try {
+            long time = 1000L;
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
